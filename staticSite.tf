d22ee7781754a6804caa7ef3eb5e1e6d3d1c57d5
zachvalenta <zjv5002@gmail.com>
2017-10-08 15:17:17 -0400

    move key_name to .vars, still troubleshooting security group

--- staticSite.tf
+++ staticSite.tf
@@ -6,12 +6,20 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-# locate public key on AWS
-variable "key_name" {
-  default = "tfDemoPair"
-}
-# locate private key on client
+# public key on AWS, private key on client
+variable "key_name" {}
 variable "private_key_path" {}
+
+# sg
+variable "security_group_id" {}
+
+####################
+# DATA
+####################
+
+# data "aws_security_group" "selected" {
+#   id = "${var.security_group_id}"
+# }
 
 ####################
 # PROVIDERS
@@ -27,6 +35,12 @@ provider "aws" {
 ####################
 # RESOURCES
 ####################
+
+# https://www.terraform.io/docs/providers/aws/d/security_group.html
+# resource "aws_subnet" "subnet" {
+#   vpc_id     = "${data.aws_security_group.selected.vpc_id}"
+#   cidr_block = "10.0.1.0/24"
+# }
 
 resource "aws_instance" "nginxServer" {
   # this AMI specific to us-east-2
@@ -56,8 +70,4 @@ resource "aws_instance" "nginxServer" {
 
 output "dns" {
     value = "${aws_instance.nginxServer.public_dns}"
-}
-
-output "ip" {
-    value = "${aws_instance.nginxServer.public_ip}"
 }
