1d5b177a9c2920d01c7edc669eb7955b347e3284
zachvalenta <zjv5002@gmail.com>
2017-10-13 00:18:36 -0400

    it was the ami

--- staticSite.tf
+++ staticSite.tf
@@ -6,7 +6,7 @@ provider "aws" {
   # authenticate IAM user
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-east-2"
+  region     = "us-east-1"
 }
 
 ####################
@@ -15,9 +15,13 @@ provider "aws" {
 
 resource "aws_instance" "nginxServer" {
   # this AMI specific to us-east-2
-  ami           = "ami-3883a55d"
+  ami           = "ami-8c1be5f6"
   instance_type = "t2.micro"
   key_name      = "${var.key_name}"
+  security_groups= ["launch-wizard-1"]
+  tags {
+      Name = "tfDemoUpdatedAMI"
+  }
 
   # connection {
   #   # default user for all EC2 instances
