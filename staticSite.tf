4585131c3527f3c0492c66764f0e1976cd7e6050
zachvalenta <zjv5002@gmail.com>
2017-10-19 22:59:52 -0400

    add link to debug fix

--- staticSite.tf
+++ staticSite.tf
@@ -3,7 +3,7 @@
 ####################
 
 provider "aws" {
-  # authenticate IAM user
+# authenticate IAM user
   access_key      = "${var.aws_access_key}"
   secret_key      = "${var.aws_secret_key}"
   region          = "us-east-1"
@@ -23,6 +23,7 @@ resource "aws_security_group" "ssh_only" {
     protocol      = "tcp"
     cidr_blocks   = ["0.0.0.0/0"]
   }
+# debug for failed yum update led to egress rule [https://stackoverflow.com/questions/20822021/amazon-ec2-instance-cant-update-or-use-yum]
   egress {
     from_port     = 0
     to_port       = 0
@@ -33,7 +34,7 @@ resource "aws_security_group" "ssh_only" {
 }
 
 resource "aws_instance" "nginxServer" {
-  # AMI specific to us-east-1
+# AMI specific to us-east-1
   ami             = "ami-8c1be5f6"
   instance_type   = "t2.micro"
   key_name        = "${var.key_name}"
@@ -43,11 +44,11 @@ resource "aws_instance" "nginxServer" {
   }
 
   connection {
-    # default user for all EC2 instances
+  # default user for all EC2 instances
     user          = "ec2-user"
     timeout       = "30s"
-    # private_key = "${file("/Users/zach/Desktop/aws_creds/EC2/ZVregion1Default.pem")}"
-    # https://github.com/hashicorp/terraform/issues/9308
+  # private_key = "${file("/Users/zach/Desktop/aws_creds/EC2/ZVregion1Default.pem")}"
+  # https://github.com/hashicorp/terraform/issues/9308
     private_key   = "${file(var.private_key_path)}"
   }
 
